567b257b7b112aa73a55c999f590c23a34106053
Yashar Js <Yashar@Icys-MacBook-Pro.local>
2022-03-15 23:21:36 -0400

    user model tested

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
-//const {User } = require('../../models');
+const {User } = require('../../models');
 
 router.get('/',(req,res)=>{
-
+    User.findAll().then(dbUserData => res.json(dbUserData)).catch(err => res.status(500).json(err));
 })
 
-
+router.post('/',(req,res)=>{
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData=>{
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
 module.exports = router;
