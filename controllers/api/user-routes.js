17006cf20379694c8083f4618f0a35f9f03caf01
Yashar Js <Yashar@Icys-MacBook-Pro.local>
2022-03-15 22:52:50 -0400

    connect routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+//const {User } = require('../../models');
+
+router.get('/',(req,res)=>{
+
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.use(routes);
 
 sequelize.sync({force: false}).then(()=>{
     app.listen(PORT, ()=>console.log(`Now listening ${PORT}`));
